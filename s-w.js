3159df9bb0e7da7b2896f293c73dc77796eedca8
VladimirPiriz <vladi_exe_piriz@hotmail.com>
2021-06-01 20:27:49 -0300

    addlistener

--- s-w.js
+++ s-w.js
@@ -1,17 +1,28 @@
 const CACHE_NAME = 'offline';
 const OFFLINE_URL = 'offline.html';
 
+// self.addEventListener('install', function(event) {
+//     console.log('[ServiceWorker] Install');
+  
+//     event.waitUntil((async () => {
+//         const cache = await caches.open(CACHE_NAME);
+//         // Setting {cache: 'reload'} in the new request will ensure that the response
+//         // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+//         await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+//     })());
+  
+//     self.skipWaiting();
+// });
+
 self.addEventListener('install', function(event) {
-    console.log('[ServiceWorker] Install');
-  
-    event.waitUntil((async () => {
-        const cache = await caches.open(CACHE_NAME);
-        // Setting {cache: 'reload'} in the new request will ensure that the response
-        // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-        await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
-    })());
-  
-    self.skipWaiting();
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
 });
 
 self.addEventListener('activate', (event) => {
